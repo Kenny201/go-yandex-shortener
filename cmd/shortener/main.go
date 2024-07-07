8c65444bbd90eec1dc6923314317d98a296ab691
vitaly izmestjev <vitaly.izmestjev@yandex.ru>
2024-07-07 12:13:23 +0300

    feat(incr1): edit error handling

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,20 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"github.com/Kenny201/go-yandex-shortener.git/internal/app/url"
 	"github.com/Kenny201/go-yandex-shortener.git/internal/http"
 	"log"
 )
 
 func main() {
-	us, err := url.NewService(url.WithMemoryRepository())
-
-	if err != nil {
-		fmt.Printf("%v", err)
-
-		return
-	}
+	us := url.NewService(url.WithMemoryRepository())
 
 	urlHandler := http.NewURLHandler(us)
 
