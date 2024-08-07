d5a09b32c18afcb835a3ae12fc6a127312166c5c
Vitalik <panins899@gmail.com>
2024-08-07 14:57:13 +0300

    feat(incr6): add logger middleware (#11)
    feat(incr6): add logger middleware (#11)
    
    Co-authored-by: vitaly izmestjev <vitaly.izmestjev@yandex.ru>

--- internal/http/routes.go
+++ internal/http/routes.go
@@ -1,12 +1,15 @@
 package http
 
 import (
+	"github.com/Kenny201/go-yandex-shortener.git/internal/http/middleware"
 	"github.com/go-chi/chi/v5"
 )
 
 // Routes returns the initialized router
 func (s *Server) useRoutes() *chi.Mux {
 	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
 	r.Post("/", s.shortenerHandler.PostHandler)
 	r.Get("/{id}", s.shortenerHandler.GetByIDHandler)
 
