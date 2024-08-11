d89424a3cda48a07dda67ab899f238fa0b14ab4e
vitaly izmestjev <vitaly.izmestjev@yandex.ru>
2024-08-11 23:07:42 +0300

    feat(incr13): add unique index for original_url

--- internal/migrations/1_add_shorteners_table.up.sql
+++ internal/migrations/1_add_shorteners_table.up.sql
@@ -2,5 +2,5 @@ CREATE TABLE IF NOT EXISTS shorteners
 (
     id           VARCHAR PRIMARY KEY,
     short_url    VARCHAR NOT NULL,
-    original_url VARCHAR NOT NULL
+    original_url VARCHAR NOT NULL UNIQUE
 );
