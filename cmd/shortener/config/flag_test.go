87ef07d306fad164b5931fb0c76808b71170d903
vitaly izmestjev <vitaly.izmestjev@yandex.ru>
2024-08-12 23:16:45 +0300

    feat(incr13): deleted unnecessary test TestFlagsWithError from flag_test

--- cmd/shortener/config/flag_test.go
+++ cmd/shortener/config/flag_test.go
@@ -16,66 +16,6 @@ const (
 	URL = "http://localhost:8080"
 )
 
-func TestFlagsWithError(t *testing.T) {
-	tests := []struct {
-		name           string
-		body           string
-		args           []string
-		wantError      string
-		wantStatusCode int
-	}{
-		{
-			name: "incorrect base_url:not set scheme into argument shortener_base_url",
-			body: "https://yandex.ru",
-			args: []string{
-				"-a", "http://localhost:8080",
-				"-b", "://localhost:8080",
-			},
-			wantError:      "failed to parse base url\n",
-			wantStatusCode: http.StatusBadRequest,
-		},
-		{
-			name: "incorrect base_url:not set port into argument shortener_base_url",
-			body: "https://practicum.yandex.ru",
-			args: []string{
-				"-a", "http://localhost:8080",
-				"-b", "http://localhost",
-			},
-			wantError:      "failed to split host and port\n",
-			wantStatusCode: http.StatusBadRequest,
-		},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			args := initArgs(t, tt.args)
-
-			rw, r := sendRequest(http.MethodPost, URL, strings.NewReader(tt.body))
-			repository := storage.NewMemoryShortenerRepository(args.BaseURL)
-			linkShortener := shortener.New(repository)
-
-			handler.New(linkShortener).Post(rw, r)
-
-			res := rw.Result()
-			defer responseClose(t, res)
-
-			body, err := io.ReadAll(res.Body)
-
-			if err != nil {
-				t.Fatalf("could not read response:%v", err)
-			}
-
-			if string(body) != tt.wantError {
-				t.Errorf("error handler not correct: got %v want %v", string(body), tt.wantError)
-			}
-
-			if res.StatusCode != tt.wantStatusCode {
-				t.Errorf("excpected status: got %v want %v", res.StatusCode, tt.wantStatusCode)
-			}
-		})
-	}
-}
-
 func TestFlags(t *testing.T) {
 	tests := []struct {
 		name           string
