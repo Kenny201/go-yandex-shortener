451878ff7bce6d69b651a62616152e61af5718c6
vitaly izmestjev <vitaly.izmestjev@yandex.ru>
2024-09-04 17:54:26 +0300

    feat(incr15): edited routes

--- internal/http/routes.go
+++ internal/http/routes.go
@@ -1,9 +1,10 @@
 package http
 
 import (
+	"github.com/go-chi/chi/v5"
+
 	"github.com/Kenny201/go-yandex-shortener.git/internal/http/handler"
 	"github.com/Kenny201/go-yandex-shortener.git/internal/http/middleware"
-	"github.com/go-chi/chi/v5"
 )
 
 func useRoutes(handler handler.Handler) *chi.Mux {
@@ -19,10 +20,14 @@ func useRoutes(handler handler.Handler) *chi.Mux {
 	r.Get("/ping", handler.Ping)
 
 	r.Route("/api", func(r chi.Router) {
-		r.With(middleware.AuthMiddleware()).Post("/shorten", handler.PostAPI)
-		r.With(middleware.AuthMiddleware()).Post("/shorten/batch", handler.PostBatch)
-		r.With(middleware.AuthCheckMiddleware()).Get("/user/urls", handler.GetAll)
-		r.With(middleware.AuthCheckMiddleware()).Delete("/user/urls", handler.Delete)
+		r.With(middleware.AuthMiddleware()).Route("/shorten", func(r chi.Router) {
+			r.Post("/", handler.PostAPI)
+			r.Post("/batch", handler.PostBatch)
+		})
+		r.With(middleware.AuthCheckMiddleware()).Route("/user", func(r chi.Router) {
+			r.Get("/urls", handler.GetAll)
+			r.Delete("/urls", handler.Delete)
+		})
 	})
 
 	return r
